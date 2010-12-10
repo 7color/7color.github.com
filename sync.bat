5a2aa8c323592ec5c49daf9f46651a7fbe1634a8
7color <voow@qq.com>
2010-12-11 02:41:37 +0800

    Automated commit on 2010/12/11 2:41:25

--- sync.bat
+++ sync.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 title Git Sync... 
 ::Start...
 echo Start synchronizing...
@@ -6,9 +6,14 @@ echo Start synchronizing...
 echo Commit changes...
 
 :: get date and time 
-for /f "delims=" %%a in ('date/t') do @set mydate=%%a 
-for /f "delims=" %%a in ('time/t') do @set mytime=%%a 
+:: for /f "delims=" %%a in ('date/t') do @set mydate=%%a 
+:: for /f "delims=" %%a in ('time/t') do @set mytime=%%a 
+set mydate=%DATE:~0,10%
+set mytime=%TIME:~0,8%
 set fvar=%mydate%%mytime% 
+
+echo %fval%
+pause
 
 :: add all new files 
 call git add . 
