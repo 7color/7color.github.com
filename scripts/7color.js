848e00afddec45048646271cd4108c1cdcffeaed
xixer <xixer@126.com>
2010-12-27 15:36:23 +0800

    Automatic commit at 2010/12/27 15:36:23

--- scripts/7color.js
+++ scripts/7color.js
@@ -425,4 +425,19 @@ $(function(){
             });
 		});
     }();
+	//百度统计
+	var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+	document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fae3873e8cd3a7eb735164e586ffa3150' type='text/javascript'%3E%3C/script%3E"));
+	
+	//谷歌统计
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-20432999-1']);
+	_gaq.push(['_setDomainName', '.7color.org']);
+	_gaq.push(['_trackPageview']);
+	
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 });
