5ab430b46691dd16d4abfe7a7740afa0a20739c5
xixer <xixer@126.com>
2013-01-31 00:40:09 +0800

    Automatic commit at 2013/01/31 00:40:09

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ echo "========================================"
 read -p "Pull from server completed, continue to push(y/n)?" push_flag
 if [ "$push_flag" != "n" ]; then
     read -p "Enter commit:" commit
-    if [ -z $commit ]; then
+    if [ -z "$commit" ]; then
         mydate=`date '+%x %T'`
         commit="Automatic commit at $mydate"
     else
